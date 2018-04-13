def219fa607d2c7e0a96ce85f47542d3b6aaff06
LiamZ <liamz95@outlook.com>
2018-04-13 10:17:56 +1000

    Initial Commit

---
+++ AHQ/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AHQ
+//
+//  Created by LIYU ZHANG on 13/4/18.
+//  Copyright © 2018 liyuzhang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
