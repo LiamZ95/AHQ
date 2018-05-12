cea115e239551dd18f5f7b05b7e4020a0426a5c5
LiamZ <liamz95@outlook.com>
2018-05-12 20:23:25 +1000

    ready to integrate unity

---
+++ AHQ/Controller/UnityViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UnityViewController.swift
+//  AHQ
+//
+//  Created by LIYU ZHANG on 12/5/18.
+//  Copyright © 2018 liyuzhang. All rights reserved.
+//
+
+import UIKit
+
+class UnityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
